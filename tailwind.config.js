4dc5d963fd0c569fd8e6294a14fa3b341a702775
Nshuti7 <bricekabanda8@gmail.com>
2025-06-03 11:30:58 +0300

    added nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}", // if you ever put JSX in App.js/tsx
+    "./app/**/*.{js,jsx,ts,tsx}", // include all of your Expo Router screens/components
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
   },
   plugins: [],
-}
+};
